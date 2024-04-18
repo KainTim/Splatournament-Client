4e28c82830fbff963d4c21ff5b862981ab98eb84
Tim Kainz <kainzt21030@sus.htl-grieskirchen.at>
2024-04-18 08:33:16 +0200

    added LoginFragment and added ViewBinding to build.gradle.kts

---
+++ app/src/main/java/com/kainzt/splatournament_client/fragments/LoginFragment.java
@@ -0,0 +1,33 @@
+package com.kainzt.splatournament_client.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.kainzt.splatournament_client.R;
+public class LoginFragment extends Fragment {
+
+    public LoginFragment() {
+        // Required empty public constructor
+    }
+
+    public static LoginFragment newInstance() {
+        return new LoginFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login_fragmnet, container, false);
+    }
+}
