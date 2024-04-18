9b13ff4f7b4d0a1fc6b7649eab0bea4cd75b2da2
Tim Kainz <kainzt21030@sus.htl-grieskirchen.at>
2024-04-18 10:05:13 +0200

    added Fragment switching to MainActivity

--- app/src/main/java/com/kainzt/splatournament_client/MainActivity.java
+++ app/src/main/java/com/kainzt/splatournament_client/MainActivity.java
@@ -7,8 +7,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.kainzt.splatournament_client.fragments.HomeFragment;
+import com.kainzt.splatournament_client.fragments.LoginFragment;
+import com.kainzt.splatournament_client.fragments.RegisterFragment;
 import com.kainzt.splatournament_client.viewmodels.MainViewModel;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,7 +28,18 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
         MainViewModel viewModel = new ViewModelProvider(this).get(MainViewModel.class);
-        viewModel.state.observe(this,integer ->{
+        viewModel.state.observe(this,state ->{
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            switch (state){
+                case 0: fragmentTransaction.replace(R.id.clMain, LoginFragment.newInstance());
+                    break;
+                case 1: fragmentTransaction.replace(R.id.clMain, RegisterFragment.newInstance());
+                    break;
+                case 2: fragmentTransaction.replace(R.id.clMain, HomeFragment.newInstance());
+                    break;
+                default: fragmentTransaction.replace(R.id.clMain,LoginFragment.newInstance());
+            }
+            fragmentTransaction.commit();
         });
     }
 }
